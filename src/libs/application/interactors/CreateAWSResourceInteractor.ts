98b59d3adbc6db88f223ebaeace33c490cac049a
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2020-05-27 16:04:15 +0900

    Application Interactor: CreateAWSResourceInteractor

---
+++ src/libs/application/interactors/CreateAWSResourceInteractor.ts
@@ -0,0 +1,20 @@
+import { inject } from "inversify";
+
+import { TYPES } from "../../types";
+import { IAWSState } from "../../domain/state/aws";
+import { VPCRepository } from "../../domain/repositories/aws";
+import { CreateAWSResourceUseCase } from "../usecases/CreateAWSResourceUseCase";
+
+export class CreateAWSResourceInteractor extends CreateAWSResourceUseCase {
+  private _vpcRepo: VPCRepository;
+
+  constructor(@inject(TYPES.VPCRepository) vpcRepo: VPCRepository) {
+    super();
+    this._vpcRepo = vpcRepo;
+  }
+
+  execute = async (resources: Omit<IAWSState, "metadata">): Promise<void> => {
+    const vpcPromises = resources.vpcList.map(vpc => this._vpcRepo.create(vpc));
+    await Promise.all(vpcPromises);
+  };
+}
