5fb97c1ca4ab983995114230dbe72044dadba525
tenmakamatani <tenmakamatani0@gmail.com>
2020-06-16 13:10:11 +0900

    Delete unuse code

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,5 @@
 import * as React from "react";
-import { Rnd } from "react-rnd";
-
-import SVG from "../public/Amazon-EC2.svg";
 
 export const App = () => {
-  return (
-    <div>
-      <Rnd
-        lockAspectRatio={true}
-        style={{
-          cursor: "pointer"
-        }}
-      >
-        <div>
-          <img
-            style={{ width: "100%", height: "100%", pointerEvents: "none" }}
-            src={SVG}
-            alt=""
-          />
-        </div>
-      </Rnd>
-    </div>
-  );
+  return <div></div>;
 };
