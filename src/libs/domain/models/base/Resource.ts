52610e555182115e5c20677ea398b36c86b5db27
tenmakamatani <tenmakamatani0@gmail.com>
2020-06-17 10:47:48 +0900

    Add comments

--- src/libs/domain/models/base/Resource.ts
+++ src/libs/domain/models/base/Resource.ts
@@ -1,6 +1,7 @@
 import { Provider } from "./Provider";
 
 // 全てのリソースの抽象として使用する
+// Resource<T>はジェネリクスとして使えないのでAllResourceに分離
 export abstract class AllResource {}
 
 export abstract class Resource<T> extends AllResource {
