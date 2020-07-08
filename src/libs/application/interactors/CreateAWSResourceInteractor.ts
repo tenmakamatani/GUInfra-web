b533b325c3e9bde55d707d3437968d42885bdc59
tenmakamatani <tenmakamatani0@gmail.com>
2020-07-08 12:29:43 +0900

    Update: CreateAWSResourceInteractor to create EC2

--- src/libs/application/interactors/CreateAWSResourceInteractor.ts
+++ src/libs/application/interactors/CreateAWSResourceInteractor.ts
@@ -2,20 +2,26 @@ import { inject, injectable } from "inversify";
 
 import { TYPES } from "../../types";
 import { IAWSState } from "../../domain/state/aws";
-import { VPCRepository } from "../../domain/repositories/aws";
+import { EC2Repository, VPCRepository } from "../../domain/repositories/aws";
 import { CreateAWSResourceUseCase } from "../usecases/CreateAWSResourceUseCase";
 
 @injectable()
 export class CreateAWSResourceInteractor extends CreateAWSResourceUseCase {
+  private _ec2Repo: EC2Repository;
   private _vpcRepo: VPCRepository;
 
-  constructor(@inject(TYPES.VPCRepository) vpcRepo: VPCRepository) {
+  constructor(
+    @inject(TYPES.EC2Repository) ec2Repo: EC2Repository,
+    @inject(TYPES.VPCRepository) vpcRepo: VPCRepository
+  ) {
     super();
+    this._ec2Repo = ec2Repo;
     this._vpcRepo = vpcRepo;
   }
 
   execute = async (resources: Omit<IAWSState, "metadata">): Promise<void> => {
+    const ec2Promises = resources.ec2List.map(ec2 => this._ec2Repo.create(ec2));
     const vpcPromises = resources.vpcList.map(vpc => this._vpcRepo.create(vpc));
-    await Promise.all(vpcPromises);
+    await Promise.all([ec2Promises, vpcPromises]);
   };
 }
