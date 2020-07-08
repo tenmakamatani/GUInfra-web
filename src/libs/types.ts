fc2e40f729c0011b8da0e90201fbccf0a04a475f
tenmakamatani <tenmakamatani0@gmail.com>
2020-07-08 12:27:43 +0900

    Update: DI TYPES

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,4 +1,5 @@
 export const TYPES = {
+  EC2Repository: Symbol.for("EC2Repository"),
   VPCRepository: Symbol.for("VPCRepository"),
   CreateAWSResourceUseCase: Symbol.for("CreateAWSResourceUseCase")
 } as const;
