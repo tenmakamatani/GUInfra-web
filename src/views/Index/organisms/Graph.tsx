8f0e3b00354a6dc354e922f69a79d0311c2649f0
tenmakamatani <tenmakamatani0@gmail.com>
2020-07-08 22:21:22 +0900

    Update: To create ec2

--- src/views/Index/organisms/Graph.tsx
+++ src/views/Index/organisms/Graph.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 
 import { awsSelector } from "@modules/aws";
 
+import { EC2Item } from "../resources/EC2Item";
 import { VPCItem } from "../resources/VPCItem";
 
 import { styles } from "../styles";
@@ -12,6 +13,9 @@ export const Graph: React.SFC = () => {
   console.log(awsResources);
   return (
     <div css={styles.graph.wrapper}>
+      {awsResources.ec2List.map(ec2 => (
+        <EC2Item ec2={ec2} />
+      ))}
       {awsResources.vpcList.map(vpc => (
         <VPCItem vpc={vpc} />
       ))}
---
+++ src/views/Index/resources/EC2Item.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { useDispatch } from "react-redux";
+
+import EC2 from "../../../../public/aws/EC2.png";
+
+import { awsActions } from "@modules/aws";
+import { IEC2View } from "@modules/aws/resources";
+import { uiActions, ModalTypes } from "@modules/ui";
+
+import { EntityResourceItem } from "../atoms/EntityResourceItem";
+
+interface IProps {
+  ec2: IEC2View;
+}
+
+export const EC2Item: React.SFC<IProps> = ({ ec2 }) => {
+  const dispatch = useDispatch();
+  return (
+    <EntityResourceItem
+      key={ec2.resource.id.value}
+      source={EC2}
+      resource={ec2}
+      onDragStop={(e, data) => {
+        if (Math.abs(data.x - ec2.x) < 5 && Math.abs(data.y - ec2.y) < 5) {
+          dispatch(
+            uiActions.appearModal({
+              type: ModalTypes.EC2Form,
+              resource: ec2.resource
+            })
+          );
+          return;
+        }
+        dispatch(
+          awsActions.ec2.update({
+            ec2Id: ec2.resource.id,
+            ec2View: {
+              x: data.x,
+              y: data.y
+            }
+          })
+        );
+      }}
+      onResizeStop={(e, dir, elementRef, delta, position) => {
+        if (delta.width === ec2.width && delta.height === ec2.height) {
+          return;
+        }
+        dispatch(
+          awsActions.ec2.update({
+            ec2Id: ec2.resource.id,
+            ec2View: {
+              width: delta.width,
+              height: delta.height
+            }
+          })
+        );
+      }}
+    />
+  );
+};
