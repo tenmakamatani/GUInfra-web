7488e28c8cfb48924a43a0e2e1b13af24ee4e38b
tenmakamatani <tenmakamatani0@gmail.com>
2020-07-08 12:31:58 +0900

    Update: Add EC2Repository to DI

--- src/libs/application/DI.ts
+++ src/libs/application/DI.ts
@@ -2,8 +2,8 @@ import { Container } from "inversify";
 
 import { TYPES } from "../types";
 import { IAWSState } from "../domain/state/aws";
-import { VPCRepository } from "../domain/repositories/aws";
-import { SdkVPCRepository } from "../infrastructure/aws";
+import { EC2Repository, VPCRepository } from "../domain/repositories/aws";
+import { SdkEC2Repository, SdkVPCRepository } from "../infrastructure/aws";
 import { CreateAWSResourceUseCase } from "./usecases/CreateAWSResourceUseCase";
 import { CreateAWSResourceInteractor } from "./interactors/CreateAWSResourceInteractor";
 
@@ -11,6 +11,9 @@ const container = new Container();
 
 export class DI {
   static setup(metadata: IAWSState["metadata"]) {
+    container
+      .bind<EC2Repository>(TYPES.EC2Repository)
+      .toConstantValue(new SdkEC2Repository(metadata));
     container
       .bind<VPCRepository>(TYPES.VPCRepository)
       .toConstantValue(new SdkVPCRepository(metadata));
@@ -20,6 +23,9 @@ export class DI {
       .inSingletonScope();
   }
 
+  static get ec2Repo() {
+    return container.get<EC2Repository>(TYPES.EC2Repository);
+  }
   static get vpcRepo() {
     return container.get<VPCRepository>(TYPES.VPCRepository);
   }
