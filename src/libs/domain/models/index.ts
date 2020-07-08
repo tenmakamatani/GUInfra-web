093e82144f277f20bc967514d7ce7f8d37de4070
tenmakamatani <tenmakamatani0@gmail.com>
2020-07-08 12:17:23 +0900

    Update: Add EC2 to AllResource and EntityResource

--- src/libs/domain/models/index.ts
+++ src/libs/domain/models/index.ts
@@ -1,4 +1,5 @@
-import { VPC } from "./aws";
+import { EC2, VPC } from "./aws";
 
-export type AllResource = VPC;
+export type AllResource = EC2 | VPC;
+export type EntityResource = EC2;
 export type ScopeResource = VPC;
