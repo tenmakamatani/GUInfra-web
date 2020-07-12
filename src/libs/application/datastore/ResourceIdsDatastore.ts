c8fb7aad21f8d30f94441dc3cf8f1c362ca24d11
tenmakamatani <tenmakamatani0@gmail.com>
2020-07-12 22:44:27 +0900

    Fix: Set default values to ResourceIdsDatastore members

--- src/libs/application/datastore/ResourceIdsDatastore.ts
+++ src/libs/application/datastore/ResourceIdsDatastore.ts
@@ -1,6 +1,6 @@
 export class ResourceIdsDatastore {
-  public static vpcIds: string[];
-  public static ec2Ids: string[];
+  public static vpcIds: string[] = [];
+  public static ec2Ids: string[] = [];
 
   static freshAll = (): void => {
     ResourceIdsDatastore.ec2Ids = [];
