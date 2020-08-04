713678239d798f96cf231f0d88432d41d7c3c923
tenmakamatani <tenmakamatani0@gmail.com>
2020-08-04 13:00:08 +0900

    Update: Styles of Input

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
+import styles from "./styles";
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
 export const Input: React.SFC<Props> = (props) => {
   return (
-    <input {...props} />
+    <input
+      css={styles.input}
+      {...props}
+    />
   )
 }
