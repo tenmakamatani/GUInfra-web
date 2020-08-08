ea6648e350c04c05a1949d99190daa7deb78533e
tenmakamatani <tenmakamatani0@gmail.com>
2020-08-08 22:54:54 +0900

    Create: domain model RouteTable

---
+++ src/libs/domain/models/aws/RouteTable.ts
@@ -0,0 +1,19 @@
+import { Id } from "../base";
+import { AWSResource, ITag } from "./common/AWSResource";
+import { VPCId, InternetGatewayId } from ".";
+
+interface IRouteTable {
+  vpcId: VPCId;
+  gatewayId: InternetGatewayId;
+}
+
+export class RouteTable extends AWSResource<IRouteTable> {
+  readonly id: RouteTableId;
+
+  constructor(init: { id?: string; properties: IRouteTable; tags: ITag[] }) {
+    super(init);
+    this.id = new RouteTableId(init.id);
+  }
+}
+
+export class RouteTableId extends Id {}
--- src/libs/domain/models/aws/index.ts
+++ src/libs/domain/models/aws/index.ts
@@ -1,5 +1,6 @@
 export * from "./EC2";
 export * from "./VPC";
 export * from "./Subnet";
+export * from "./RouteTable";
 export * from "./SecurityGroup";
 export * from "./InternetGateway";
