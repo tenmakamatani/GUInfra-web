2692cf1e1275b6c3e119b6c97294551e5fd0e698
tenmakamatani <tenmakamatani0@gmail.com>
2020-08-09 12:26:58 +0900

    Update: TYPES

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,6 +1,11 @@
 export const TYPES = {
+  // Repository
   EC2Repository: Symbol.for("EC2Repository"),
   VPCRepository: Symbol.for("VPCRepository"),
+  SubnetRepository: Symbol.for("SubnetRepository"),
+  RouteTableRepository: Symbol.for("RouteTableRepository"),
   SecurityGroupRepository: Symbol.for("SecurityGroupRepository"),
+  InternetGatewayRepository: Symbol.for("InternetGatewayRepository"),
+  // UseCase
   AWSResourceUseCase: Symbol.for("AWSResourceUseCase")
 } as const;
