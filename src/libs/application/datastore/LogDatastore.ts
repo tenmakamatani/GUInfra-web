6f13d37b9813b3f4b6746b35dda58caef4ff917b
tenmakamatani <tenmakamatani0@gmail.com>
2020-08-10 22:07:20 +0900

    Update: LogDatastore method

--- src/libs/application/datastore/LogDatastore.ts
+++ src/libs/application/datastore/LogDatastore.ts
@@ -1,7 +1,28 @@
-export class LogDatastore {
-  public static executionLog: string[] = [];
+type LogType = "normal" | "error";
+interface ILog {
+  value: string;
+  type: LogType;
+}
 
+export class LogDatastore {
+  public static values: ILog[] = [];
+
+  static getLog(): ILog[] {
+    return LogDatastore.values;
+  }
+  static normal(value: string): void {
+    LogDatastore.values.push({
+      value: value,
+      type: "normal"
+    });
+  }
+  static error(value: string): void {
+    LogDatastore.values.push({
+      value: value,
+      type: "error"
+    });
+  }
   static fresh(): void {
-    LogDatastore.executionLog = [];
+    LogDatastore.values = [];
   }
 }
