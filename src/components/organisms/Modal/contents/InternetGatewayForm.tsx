45fa0d9e5bdee404b147d7ca60f7780dd0f1b0fa
tenmakamatani <tenmakamatani0@gmail.com>
2020-08-11 23:33:31 +0900

    Create: InternetGatewayForm

---
+++ src/components/organisms/Modal/contents/InternetGatewayForm.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { useDispatch } from "react-redux";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
+import { InternetGateway } from "@libs/domain/models/aws";
+
+import { uiActions } from "@modules/ui";
+import { awsActions } from "@modules/aws";
+
+import { Button } from "@components/atoms";
+
+interface IProps {
+  internetGateway?: InternetGateway;
+}
+
+interface IFormValues {}
+
+const validation = Yup.object().shape<IFormValues>({});
+
+export const InternetGatewayForm: React.SFC<IProps> = ({ internetGateway }) => {
+  const dispatch = useDispatch();
+  const formik = useFormik<IFormValues>({
+    validationSchema: validation,
+    initialValues: {},
+    onSubmit: values => {
+      if (internetGateway) {
+        internetGateway.update({
+          properties: values
+        });
+        dispatch(
+          awsActions.internetGateway.update({
+            internetGatewayId: internetGateway.id,
+            internetGatewayView: {
+              resource: internetGateway
+            }
+          })
+        );
+      } else {
+        dispatch(
+          awsActions.internetGateway.create({
+            resource: new InternetGateway({
+              properties: {},
+              tags: []
+            })
+          })
+        );
+      }
+      dispatch(uiActions.removeModal());
+    }
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <Button
+        type="submit"
+        value={internetGateway ? "更新" : "作成"}
+        onClick={() => {}}
+      />
+      {internetGateway ? (
+        <Button
+          value="削除"
+          onClick={() => {
+            dispatch(awsActions.internetGateway.remove(internetGateway.id));
+            dispatch(uiActions.removeModal());
+          }}
+        />
+      ) : null}
+    </form>
+  );
+};
