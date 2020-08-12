1c5a9aacef5cb2e95f240f185254971e9eb8deb2
tenmakamatani <tenmakamatani0@gmail.com>
2020-08-12 12:01:15 +0900

    Update: display RouteTableItem

--- src/components/pages/Index/molecules/AttachResourceItemList.tsx
+++ src/components/pages/Index/molecules/AttachResourceItemList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useSelector } from "react-redux";
 import { awsSelector } from "@modules/aws";
+import { RouteTableItem } from "../resources/aws/RouteTableItem";
 import { SecurityGroupItem } from "../resources/aws/SecurityGroupItem";
 import { styles } from "../styles";
 
@@ -9,9 +10,13 @@ export const AttachResourceItemList = () => {
   return (
     <div css={styles.attachResourceItemList.wrapper}>
       <div>
-        <h3>Security Group</h3>
+        <h3>SecurityGroup</h3>
         {allResources.securityGroupList.map(s => (
           <SecurityGroupItem securityGroup={s} />
+        ))}
+        <h3>RouteTable</h3>
+        {allResources.routeTableList.map(r => (
+          <RouteTableItem routeTable={r} />
         ))}
       </div>
     </div>
