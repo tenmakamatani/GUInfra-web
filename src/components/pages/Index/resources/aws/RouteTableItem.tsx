b0cfb7cbe2920c0d2a3817ca466962261d510977
tenmakamatani <tenmakamatani0@gmail.com>
2020-08-20 17:07:55 +0900

    Update: use AttachResourceItem in RouteTableItem

--- src/components/pages/Index/resources/aws/RouteTableItem.tsx
+++ src/components/pages/Index/resources/aws/RouteTableItem.tsx
@@ -1,27 +1,23 @@
 import * as React from "react";
-import { useDispatch } from "react-redux";
 import { IRouteTableView } from "@modules/aws/resources";
-import { uiActions, ModalTypes } from "@modules/ui";
+import { ModalTypes } from "@modules/ui";
+import { AttachResourceItem } from "../../atoms/AttachResourceItem";
 
 interface IProps {
   routeTable: IRouteTableView;
 }
 
 export const RouteTableItem: React.SFC<IProps> = ({ routeTable }) => {
-  const dispatch = useDispatch();
   return (
-    <div
-      onClick={() => {
-        dispatch(
-          uiActions.appearModal({
-            type: ModalTypes.RouteTableForm,
-            resource: routeTable.resource
-          })
-        );
-      }}
-    >
-      <p>{routeTable.resource.properties.vpcId.value}</p>
-      <p>{routeTable.resource.properties.gatewayId?.value}</p>
-    </div>
+    <AttachResourceItem
+      title="RouteTable"
+      description={
+        routeTable.resource.properties.gatewayId?.value +
+        "\n" +
+        routeTable.resource.properties.vpcId.value
+      }
+      modalType={ModalTypes.RouteTableForm}
+      resourceView={routeTable}
+    />
   );
 };
