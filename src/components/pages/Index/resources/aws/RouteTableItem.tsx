39dfd668e6ba26dba7f050f6d913d231e7eb6bf3
tenmakamatani <tenmakamatani0@gmail.com>
2020-09-01 13:27:48 +0900

    Update: display resource name in RouteTableItem

--- src/components/pages/Index/resources/aws/RouteTableItem.tsx
+++ src/components/pages/Index/resources/aws/RouteTableItem.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { useSelector } from "react-redux";
+import { awsSelector } from "@modules/aws";
 import { IRouteTableView } from "@modules/aws/resources";
 import { ModalTypes } from "@modules/ui";
 import { AttachResourceItem } from "../../atoms/AttachResourceItem";
@@ -8,13 +10,24 @@ interface IProps {
 }
 
 export const RouteTableItem: React.SFC<IProps> = ({ routeTable }) => {
+  const vpc = useSelector(
+    awsSelector.getVpc(routeTable.resource.properties.vpcId)
+  );
+  const subnet = useSelector(
+    awsSelector.getSubnet(routeTable.resource.properties.subnetId)
+  );
+  const gateway = useSelector(
+    awsSelector.getInternetGateway(routeTable.resource.properties.gatewayId!)
+  );
   return (
     <AttachResourceItem
-      title="RouteTable"
+      title=""
       description={
-        routeTable.resource.properties.gatewayId?.value +
+        vpc.properties.name +
         "\n" +
-        routeTable.resource.properties.vpcId.value
+        subnet.properties.name +
+        "\n" +
+        gateway.properties.name
       }
       modalType={ModalTypes.RouteTableForm}
       resourceView={routeTable}
--- src/modules/aws/selectors.ts
+++ src/modules/aws/selectors.ts
@@ -1,10 +1,35 @@
 import { IStore } from "../store";
 import { IAWSState } from "./index";
+import {
+  VPCId,
+  VPC,
+  SubnetId,
+  Subnet,
+  InternetGateway,
+  InternetGatewayId
+} from "@libs/domain/models/aws";
 
 const selectAll = (store: IStore): IAWSState => {
   return store.aws;
 };
+const getVpc = (id: VPCId) => (store: IStore): VPC => {
+  return store.aws.vpcList.find(vpc => vpc.resource.id.isEqualTo(id))!.resource;
+};
+const getSubnet = (id: SubnetId) => (store: IStore): Subnet => {
+  return store.aws.subnetList.find(subnet => subnet.resource.id.isEqualTo(id))!
+    .resource;
+};
+const getInternetGateway = (id: InternetGatewayId) => (
+  store: IStore
+): InternetGateway => {
+  return store.aws.internetGatewayList.find(internetGateway =>
+    internetGateway.resource.id.isEqualTo(id)
+  )!.resource;
+};
 
 export const selectors = {
-  selectAll
+  selectAll,
+  getVpc,
+  getSubnet,
+  getInternetGateway
 };
