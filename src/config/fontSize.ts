57f5fb03e7dac9adef5ae1836aae7ba8b06c3168
tenmakamatani <tenmakamatani0@gmail.com>
2020-09-03 14:51:50 +0900

    Create: config fontSize

---
+++ src/config/fontSize.ts
@@ -0,0 +1,7 @@
+export default {
+  huge: 50,
+  big: 25,
+  normal: 22,
+  small: 20,
+  tiny: 15
+};
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
 import regexp from "./regexp";
 import images from "./images";
 import colors from "./colors";
+import fontSize from "./fontSize";
 
 export default {
   regexp,
   images,
-  colors
+  colors,
+  fontSize
 };
