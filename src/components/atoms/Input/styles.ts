ceb8184c3ff619d30e109142b38ab67c0032e1c1
tenmakamatani <tenmakamatani0@gmail.com>
2020-09-27 17:20:40 +0900

    Update: input design

--- src/components/atoms/Input/styles.ts
+++ src/components/atoms/Input/styles.ts
@@ -1,7 +1,14 @@
 import { css } from "@emotion/core";
+import config from "@config";
 
 export default {
   input: css({
-    
+    padding: "18px 5px",
+    backgroundColor: config.colors.grey,
+    border: "none",
+    outline: "none",
+    "::placeholder": {
+      color: config.colors.darkgrey
+    }
   })
-}
+};
