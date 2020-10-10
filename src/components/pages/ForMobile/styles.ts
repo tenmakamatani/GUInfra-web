92ec106a0c2a5356d9b71e5d9481891298ed9c28
tenmakamatani <tenmakamatani0@gmail.com>
2020-10-10 19:13:41 +0900

    Create: for mobile page

---
+++ src/components/pages/ForMobile/styles.ts
@@ -0,0 +1,17 @@
+import { css } from "@emotion/core";
+
+export default {
+  wrapper: css({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  }),
+  wrapperInner: css({
+    textAlign: "center",
+  }),
+  img: css({
+    width: "80%",
+    height: "auto"
+  }),
+}
