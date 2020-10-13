7b33a35ec4160d2ebbafc24b0cc90b844b8931ae
tenmakamatani <tenmakamatani0@gmail.com>
2020-10-13 21:48:41 +0900

    Create: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,7 @@ export default class GUInfraDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
           />
+          <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon"/>
           <script src="/js/blockUnload.js"></script>
         </Head>
         <body id="root">
