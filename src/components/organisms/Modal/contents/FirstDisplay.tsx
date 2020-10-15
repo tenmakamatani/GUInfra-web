7ab64f6dd0826cb6b08e157cdb1864aba4748931
tenmakamatani <tenmakamatani0@gmail.com>
2020-10-15 14:59:15 +0900

    Update: FirstDisplay sentence

--- src/components/organisms/Modal/contents/FirstDisplay.tsx
+++ src/components/organisms/Modal/contents/FirstDisplay.tsx
@@ -19,16 +19,22 @@ export const FirstDisplay: React.SFC = () => {
     <div>
       <Margin height={10} />
       <Text size="big" bold content="はじめに" />
-      <Margin height={20} />
+      <Margin height={10} />
+      <Text
+        size="normal"
+        content="GUInfraは、選択できるプロパティを制限してクラウドインフラの構成図を作成し、実際にリソースを建てて確認することでクラウドインフラを効率よく学習できるサービスです。現在はAWSに対応しています。"
+      />
+      <Margin height={5} />
       <Text
         size="normal"
         content="当サービスでは、トークンやシークレットキーを入力することでクラウドインフラのリソースを作成することが可能ですが、それらを保存することは一切ございません。"
       />
+      <Margin height={5} />
       <Text
         size="normal"
-        content="また当サービスはスマホからではなくPCから使用することを推奨します。"
+        content="またスマホからではなくPCから使用することを推奨します。"
       />
-      <Margin height={20} />
+      <Margin height={10} />
       <div css={styles.imgWrapper}>
         <img css={styles.img} src={config.images.firstDisplay} />
       </div>
